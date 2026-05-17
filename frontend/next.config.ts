aebc7d35f5545fa3da5b4d5360f5ee1b202697f5
Smasher1614 <karan810754@gmail.com>
2026-05-17 21:17:23 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {},
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        indexedDB: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
